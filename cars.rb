920dae83f0a00044216103db038b9fb0a1d47665
Jim Vickery <jimrv8@yahoo.com>
2017-10-11 16:21:25 -0500

    reading both files

--- cars.rb
+++ cars.rb
@@ -1,9 +1,23 @@
 require 'yaml'
 
+class Cars
 
-puts File.read("car_makes.txt")
-puts File.read("car_models.txt")
+    def read_makes
+        puts File.readlines("car_makes.txt")
+    end
 
+    def read_models
+        puts File.readlines("car_models.txt")
+    end
 
+    
 
+end
 
+# c1 = Cars.new
+# c1.read_makes
+# c1.read_models
+
+File.open("car_models.txt", "r").each do |f|
+    puts YAML::load(f)
+end
