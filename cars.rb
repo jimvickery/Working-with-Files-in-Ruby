991ff98921b313e5e7cb605fddc824c0a267072a
Jim Vickery <jimrv8@yahoo.com>
2017-10-14 17:43:57 -0500

    updated MVP

--- cars.rb
+++ cars.rb
@@ -1,27 +1,22 @@
-
-	# method to create car hash
-	def create_car_hash
-        makes = File.open("car_models.txt")
-        models = File.open("car_makes.txt")
-		car_hash = Hash.new
-		makes_array = makes
-		models_array = models
-		makes_array.each do |make|
-			make = make.gsub("\n", "")
-			car_hash[make] = []
-			models_array.each do |model|
-				model = model.gsub("\n", "")
-				if make[0] == model[0]
-					model[0..1] = ""
-					car_hash[make].push(model)
-				end
+def create_car_hash
+	car_models = File.readlines("car_models.txt")
+	car_makes = File.readlines("car_makes.txt")
+	car_hash = Hash.new
+	makes_array = car_makes
+	models_array = car_models
+	makes_array.each do |make|
+		make = make.gsub("\n", "")
+		car_hash[make] = []
+		models_array.each do |model|
+			model = model.gsub("\n", "")
+			if make[0] == model[0]
+				model[0..1] = ""
+				car_hash[make].push(model)
 			end
 		end
-		puts car_hash
 	end
-    
-    # call method to crate the hash
-    create_car_hash()
+	puts car_hash
+end
 
-    
-
+# call method to crate the hash
+create_car_hash
