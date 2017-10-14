c85e72726f57e693c538c92bfddf0b098502f85b
Jim Vickery <jimrv8@yahoo.com>
2017-10-14 17:12:56 -0500

    MVP

--- cars.rb
+++ cars.rb
@@ -1,23 +1,27 @@
-require 'yaml'
 
-class Cars
-
-    def read_makes
-        puts File.readlines("car_makes.txt")
-    end
-
-    def read_models
-        puts File.readlines("car_models.txt")
-    end
+	# method to create car hash
+	def create_car_hash
+        makes = File.open("car_models.txt")
+        models = File.open("car_makes.txt")
+		car_hash = Hash.new
+		makes_array = makes
+		models_array = models
+		makes_array.each do |make|
+			make = make.gsub("\n", "")
+			car_hash[make] = []
+			models_array.each do |model|
+				model = model.gsub("\n", "")
+				if make[0] == model[0]
+					model[0..1] = ""
+					car_hash[make].push(model)
+				end
+			end
+		end
+		puts car_hash
+	end
+    
+    # call method to crate the hash
+    create_car_hash()
 
     
 
-end
-
-# c1 = Cars.new
-# c1.read_makes
-# c1.read_models
-
-File.open("car_models.txt", "r").each do |f|
-    puts YAML::load(f)
-end
